7964779019b0661188b4010cdfd51722f23d4017
Acxel Morales <acxelmorales97@gmail.com>
2023-04-27 21:00:17 -0600

    Se agrego la entidad productos, se creo el dto y se agrego la primera creacion de un producto

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -7,8 +7,36 @@ export class Product {
   id: number;
 
   @Column('text', {
-    unique: true
+    unique: true,
   })
   title: string;
+
+  @Column('float', {
+    default: 0,
+  })
+  price: number;
+
+  @Column('text', {
+    nullable: true,
+  })
+  description: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  slug: string;
+
+  @Column('int', {
+    default: 0,
+  })
+  stock: number;
+
+  @Column('text', {
+    array: true,
+  })
+  sizes: string[];
+
+  @Column('text')
+  gender: string;
 
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
+import { Product } from './entities/product.entity';
+
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>
+  ) {}
+
+  async create(createProductDto: CreateProductDto): Promise<Product> {
+    try {
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+
+      return product;
+    } catch (error) {
+      console.log(error)
+      throw new InternalServerErrorException('Help');
+    }
   }
 
   findAll() {
@@ -23,4 +43,5 @@ export class ProductsService {
   remove(id: number) {
     return `This action removes a #${id} product`;
   }
+
 }
