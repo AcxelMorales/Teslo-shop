b8c94233de98f11eaadd99b7b55f6036fd58af50
Acxel Morales <acxelmorales97@gmail.com>
2023-05-07 13:29:31 -0600

    Se agrego relacion oneToMany y manyToOne

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -3,8 +3,11 @@ import {
   BeforeUpdate,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+
+import { ProductImage } from './product-image.entity';
 
 @Entity()
 export class Product {
@@ -50,6 +53,13 @@ export class Product {
     default: []
   })
   tags: string[];
+
+  @OneToMany(
+    () => ProductImage,
+    (productImage) => productImage.product,
+    { cascade: true }
+  )
+  images?: ProductImage;
 
   @BeforeInsert()
   insertSlug(): void {
