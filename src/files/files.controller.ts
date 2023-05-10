3bbd8d24bcc786272463fdf782aef5ad88aee831
Acxel Morales <acxelmorales97@gmail.com>
2023-05-09 23:11:15 -0600

    Se agrego endpoint para recuperar la imagen, se agrego secureUrl y mejoras generales

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,13 +1,18 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 
 import { FileInterceptor } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 import { diskStorage } from 'multer';
+import { Response } from 'express';
 
 import { FilesService } from './files.service';
 
@@ -17,25 +22,41 @@ import { fileNamer } from './helpers/fileNamer';
 @Controller('files')
 export class FilesController {
 
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file', {
-    fileFilter,
-    limits: {
-      fileSize: 1000000
-    },
-    storage: diskStorage({
-      destination: './static/products',
-      filename: fileNamer,
-    })
-  }))
-  uploadFile(@UploadedFile() file: Express.Multer.File): Express.Multer.File {
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter,
+      limits: {
+        fileSize: 1000000,
+      },
+      storage: diskStorage({
+        destination: './static/products',
+        filename: fileNamer,
+      }),
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File): Object {
     if (!file) {
       throw new BadRequestException('Make sure that the file is an image');
     }
 
-    return file;
+    const secureUrl = `${this.configService.get('HOST_API')}/v1/files/product/${file.filename}`;
+
+    return { secureUrl };
+  }
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Param('imageName') imageName: string,
+    @Res() res: Response,
+  ): void {
+    const path = this.filesService.getStaticProductImage(imageName);
+    return res.sendFile(path)
   }
 
 }
