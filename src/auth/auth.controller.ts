7e706ba2cf79697acad29130df54b4f25c0d0e9b
Acxel Morales <acxelmorales97@gmail.com>
2023-05-16 23:12:19 -0600

    Se cambio el email por el id en el JWT y se agrego una ruta privada

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
 
@@ -20,6 +21,15 @@ export class AuthController {
   @Post('signin')
   loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
+  }
+
+  @Get('private')
+  @UseGuards(AuthGuard())
+  testingPrivateRoute(): Object {
+    return {
+      ok: true,
+      message: 'Hola mundo private',
+    };
   }
 
 }
