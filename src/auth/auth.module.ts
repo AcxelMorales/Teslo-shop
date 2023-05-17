bbdf4e9a9f961b60cf8b70cb4e79c289fc5576dc
Acxel Morales <acxelmorales97@gmail.com>
2023-05-16 21:18:10 -0600

    Se agrego una estrategia de JWT y se configuro como un provider

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,9 +9,11 @@ import { AuthController } from './auth.controller';
 
 import { User } from './entities/auth.entity';
 
+import { JwtStrategy } from './strategies/jwt.strategy';
+
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [
     TypeOrmModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
@@ -24,6 +26,6 @@ import { User } from './entities/auth.entity';
       }),
     }),
   ],
-  exports: [TypeOrmModule],
+  exports: [TypeOrmModule, JwtStrategy, PassportModule, JwtModule],
 })
 export class AuthModule {}
