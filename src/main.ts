6558a6fb027b0c0288d0f1ae9bccaad22921bf7f
Acxel Morales <acxelmorales97@gmail.com>
2023-05-25 03:10:18 -0600

    Se agrego libreria para swagger y se agrego configuracion inicial

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const logger = new Logger('Bootstrap')
+  const logger = new Logger('Bootstrap');
 
   app.setGlobalPrefix('api/v1');
 
@@ -16,8 +18,17 @@ async function bootstrap() {
     }),
   );
 
+  const config = new DocumentBuilder()
+    .setTitle('Teslo shop')
+    .setDescription('Teslo shop API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT);
-  logger.log(`App running on port ${process.env.PORT}`)
+  logger.log(`App running on port ${process.env.PORT}`);
 }
 
 bootstrap();
