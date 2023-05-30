da223ace3b9bda8407e85a1515427e871f1f4bb9
Acxel Morales <acxelmorales97@gmail.com>
2023-05-30 17:03:16 -0600

    Se agregaron implementaciones del socket en el archivo gateway

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -1,10 +1,22 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  WebSocketGateway,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 
 import { MessagesService } from './messages.service';
 
 @WebSocketGateway({ cors: true })
-export class MessagesGateway {
+export class MessagesGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   constructor(private readonly messagesService: MessagesService) {}
+
+  handleConnection(client: Socket): void {
+    console.log(`Cliente conectado: ${client.id}`);
+  }
+  handleDisconnect(client: any): void {
+    console.log(`Cliente desconectado: ${client.id}`);
+  }
 
 }
